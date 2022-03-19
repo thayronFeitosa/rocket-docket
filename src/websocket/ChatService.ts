92ee240ba1e2678f24207657f1140c150b4b634a
thayronFeitosa <thayronf3@gmail.com>
2022-03-19 08:14:50 -0300

    Corriguindo erro de criaçao de sala. se a sala ja estiver criada entre 2 usuarios nao deve ser permitido a criaçao de outra sala

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -3,6 +3,7 @@ import { io } from '../http'
 import { CreateChatRoomService } from '../services/CreateChatRoomService';
 import { CreateUserService } from '../services/CreateUserService';
 import { GetAllUsersService } from '../services/GetAllUsersService';
+import { GetChatRoomByUsersService } from '../services/GetChatRoomByUsersService';
 import { GetUserBySockerIdService } from '../services/GetUserBySockerIdService';
 
 io.on('connect', (socket) => {
@@ -26,11 +27,15 @@ io.on('connect', (socket) => {
     });
 
     socket.on('start_chat', async (data, callback) => {
+        const getChatRoomByUsersService = container.resolve(GetChatRoomByUsersService);
         const createChatRoomService = container.resolve(CreateChatRoomService);
         const getUserBySockerIdService = container.resolve(GetUserBySockerIdService);
-        const userLogged = await getUserBySockerIdService.execute(socket.id);
 
-        const room = await createChatRoomService.execute([data.idUser, userLogged._id]);
+        const userLogged = await getUserBySockerIdService.execute(socket.id);
+        let room = await getChatRoomByUsersService.execute([data.idUser, userLogged._id]);
+        if (!room) {
+            room = await createChatRoomService.execute([data.idUser, userLogged._id]);
+        }
         callback(room)
     })
 });
